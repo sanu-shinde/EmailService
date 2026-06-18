1d2f742a3fa519ad3a43a500ff1fa1be456ccffa
Sanu-Shinde <sanikashinde1203@gmail.com>
2026-06-18 10:50:26 +0530

    Add files via upload

---
+++ EmailSend/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using EmailSend.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmailSend.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendMail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMail(EmailModel model)
+        {
+            using (MailMessage mm = new MailMessage())
+            {
+                mm.From = new MailAddress(model.Email);
+                mm.To.Add(model.To);
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                mm.IsBodyHtml = false;
+
+                if (model.Attachment != null && model.Attachment.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(model.Attachment.FileName);
+                    mm.Attachments.Add(new Attachment(model.Attachment.InputStream, fileName));
+                }
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(model.Email, model.Password);
+
+                    smtp.Send(mm);
+                }
+            }
+
+            ViewBag.Message = "Email Send Successfully";
+            return View();
+        }
+    }
+}
+
---
+++ EmailSend/Models/EmailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmailSend.Models
+{
+    public class EmailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public HttpPostedFileBase Attachment { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
